0204c810926d4b8d096b051195e6d8f4109b8b8b
Justin Boileau <jboileau@ualberta.ca>
2021-05-23 13:15:16 -0600

    changed structure to python package

--- test.py
+++
@@ -1,9 +0,0 @@
-def reverse(astring):
-    revStr = ""
-    i = len(astring)-1 
-    while i >= 0:
-        revStr = revStr + astring[i]
-        i = i - 1
-    return revStr
-
-print(reverse('quiz 3'))
